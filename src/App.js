1a5cb289c4179ef88d80e3ce2104ffa4618fcbf9
MagmeticForce <qaz7788.br@gmail.com>
2024-10-27 18:27:31 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,52 +1,32 @@
-import logo from './logo.svg'; //notice how there is a "logo.svg" file in this "src" directory 
-                               //so we imported the logo into this script so it can be used
-import './App.css'; //notice how there is an "App.css" file in this "src" directory\
-                    //so we are using stuff from "App.css"
 
-//TO RUN THIS APP: open the Terminal and type in 'npm start'
+import React from 'react';
+import Courses from './components/Courses';
+import courses from './data/courses.json'; //"courses" is just a variable that stores the data in the .json file.
+import './App.css';
+import course_planner_page_image from './images/Image_for_Course_Planner_Page.jpg';
 
 function App() {
+  
+  //This code displays stuff on the page
+  //All the "className"s refer to the visual stylings (position, scale, etc) defined in "App.css"
+  //The code for displaying each individual course is in 'components/Courses.js'
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Semester 1
-        </p>
-        <p>
-          Semester 2
-        </p>
-        <p>
-          Semester 3
-        </p>
-      </header>
+      <img className="Course-Planner-Page-Image" alt='Stack with books, binder, and notebook.' src={course_planner_page_image}></img>
+      <h1>Your Personalized Schedule</h1>
+      <h2>Fall 2025</h2>
+
+      <div className="course-list">
+        {courses.map(course => (
+          <Courses key={course.code} course={course} />
+        ))}
+      </div>
+
+      <h2>Winter 2026</h2>
+
+
     </div>
   );
 }
-
-/*
-Template code in case it is needed:
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-*/
 
 export default App;
