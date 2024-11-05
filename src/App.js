b338682c1339cb3d3799d906fe3c4a9413861f5f
MagmeticForce <qaz7788.br@gmail.com>
2024-11-05 17:19:29 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 
 import React from 'react';
-import Courses from './components/Courses';
-import courses from './data/courses.json'; //"courses" is just a variable that stores the data in the .json file.
+import ReactDOM from 'react-dom';
 import './App.css';
 import course_planner_page_image from './images/Image_for_Course_Planner_Page.jpg';
+import { DragDropContext } from 'react-beautiful-dnd';
+import Schedule from './components/Schedule.jsx';
 
-function App() {
+
+class App extends React.Component {
+
+
+  onDragEnd = result => {
+    //TODO: add stuff
+  };
   
   //This code displays stuff on the page
   //All the "className"s refer to the visual stylings (position, scale, etc) defined in "App.css"
   //The code for displaying each individual course is in 'components/Courses.js'
-  return (
-    <div className="App">
-      <img className="Course-Planner-Page-Image" alt='Stack with books, binder, and notebook.' src={course_planner_page_image}></img>
-      <h1>Your Personalized Schedule</h1>
-      <h2>Fall 2025</h2>
-
-      <div className="course-list">
-        {courses.map(course => (
-          <Courses key={course.code} course={course} />
-        ))}
-      </div>
-
-      <h2>Winter 2026</h2>
-
-
-    </div>
-  );
+  render () {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="App">
+          <img className="Course-Planner-Page-Image" alt='Stack with books, binder, and notebook.' src={course_planner_page_image}></img>
+          <h1>Your Personalized Schedule</h1>
+            <h2>Fall 2025</h2>
+              <Schedule key="schedule_1" />
+            <h2>Winter 2026</h2>
+        </div>
+      </DragDropContext>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//export default App;
