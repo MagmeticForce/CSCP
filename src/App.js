0750b4531599fc8b6be8f5821dfc47951e58f59b
MagmeticForce <qaz7788.br@gmail.com>
2024-11-07 17:31:12 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,36 +2,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import course_planner_page_image from './images/Image_for_Course_Planner_Page.jpg';
-import { DragDropContext } from 'react-beautiful-dnd';
-import Schedule from './components/Schedule.jsx';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import IntroPage from './components/IntroPage';
+import UserInputPage from './components/UserInputPage';
+import ScheduleOutputPage from './components/ScheduleOutputPage';
 
 class App extends React.Component {
-
-
-  onDragEnd = result => {
-    //TODO: add stuff
-  };
   
   //This code displays stuff on the page
   //All the "className"s refer to the visual stylings (position, scale, etc) defined in "App.css"
   //The code for displaying each individual course is in 'components/Courses.js'
   render () {
     return (
+      <Router>
         <div className="App">
-          <img className="Course-Planner-Page-Image" alt='Stack with books, binder, and notebook.' src={course_planner_page_image}></img>
-          <h1>Your Personalized Schedule</h1>
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            <h2>Fall 2025</h2>
-              <Schedule key="schedule_1" />
-            <h2>Winter 2026</h2>
-          </DragDropContext>
+          <Routes>
+            <Route path="/" element={<IntroPage />} />
+            <Route path="/UserInputPage" element={<UserInputPage />} />
+            <Route path="/ScheduleOutputPage" element={<ScheduleOutputPage />} />
+          </Routes>
         </div>
+      </Router>
     );
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-//export default App;
+export default App;
